e43bf64e5e6cca174f91713a0d80e476c61ecdf7
Theophilus Nyarko-Mensah <nyarkotheo2@gmail.com>
2023-02-21 09:13:29 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print ten times the alphabet
+ * Return: always (0)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int i;
+	char alpha;
+
+	for (i = 0; i <= 10; i++)
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+	_putchar('\n');
+}
