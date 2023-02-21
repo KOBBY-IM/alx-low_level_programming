a615c29bb6e3bd2d239b5ec613e26cd344f338c2
Theophilus Nyarko-Mensah <nyarkotheo2@gmail.com>
2023-02-21 06:25:45 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main -print out alphabet in lowercase
+ * return: Always (0)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for  (alpha = 'a'; alpha <= 'z'; apha++)
+		putchar(alpha);
+
+
+	putchar("/n");
+}
