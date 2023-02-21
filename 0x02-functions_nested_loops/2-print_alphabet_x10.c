088cc9ff87c8bc32500c5d4d51d434de186c4c72
Theophilus Nyarko-Mensah <nyarkotheo2@gmail.com>
2023-02-21 09:28:59 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,11 +13,12 @@ void print_alphabet_x10(void)
 	char alpha;
 
 	for (i = 0; i <= 10; i++)
-
+	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
-		{
-			_putchar(alpha);
-		}
 
+			_putchar(alpha);
+
+	}
 	_putchar('\n');
+
 }
