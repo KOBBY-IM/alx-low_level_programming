04df4f216ff8affd650aa4afbb27a0245a8a55e6
Theophilus Nyarko-Mensah <nyarkotheo2@gmail.com>
2023-02-21 12:13:27 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs- function to compute the absolute value of an integer
+ * @a: n represent an integer
+ * Return: n if postive integer
+ *	-n if negative integer
+ *
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+
+}
