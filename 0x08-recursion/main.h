231ee9f79777ce16ecccf2b081152b43a95bfe4f
Theophilus Nyarko-Mensah <nyarkotheo2@gmail.com>
2023-03-08 10:38:37 -0800

    Task 1-6, 6 is not complete

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int halp(int c, int i);
+int help(int c, int i);
 int is_prime_number(int n);
 int hai(int n, int d);
 int _sqrt(int x, int i);
@@ -14,3 +14,4 @@ int get_length(char *s);
 int my_pal(char *s, int l);
 int wildcmp(char *s1, char *s2);
 int findsrc(char *s, char c, int i, int p);
+int is_prime(int n, int divisor)
