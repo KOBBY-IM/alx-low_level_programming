b1c1bddab542b3243eb8e23cec32998f673ccfe5
KOBBY-IM <nyarkotheo2@gmail.com>
2023-03-14 13:38:20 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *c;
+	char *a;
 	unsigned int i;
 
 	if (size <= 0)
 		return (NULL);
-
-
-	a = malloc(size * sizeof(a));
+	a = malloc(size * sizeof(char));
 
 	if (a == 0)
 		return (NULL);
