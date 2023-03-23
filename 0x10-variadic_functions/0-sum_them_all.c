efbf8a328a35d96eff155097f7358e0265290e1e
KOBBY-IM <nyarkotheo2@gmail.com>
2023-03-23 18:25:54 +0000

    fixed code error with sum declaration task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,8 +6,11 @@
  * @n: number of extra params
  * Return: total sum
  */
+
+
 int sum_them_all(const unsigned int n, ...)
 {
+	/*
 	unsigned int i;
 	va_list list;
 	int sum;
@@ -17,6 +20,19 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(list, int);
 
 	va_end(list);
+
+	return (sum);
+	*/
+
+	va_list nums;
+	unsigned int index, sum = 0;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
+
+	va_end(nums);
 
 	return (sum);
 }
