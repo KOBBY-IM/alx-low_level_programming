14070e4f95ddab1ebf2004c630805323e33c25fd
KOBBY-IM <nyarkotheo2@gmail.com>
2023-03-23 18:28:18 +0000

    betty errors Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	/*
-	unsigned int i;
-	va_list list;
-	int sum;
-
-	va_start(list, n);
-	for (i = 0, sum = 0; i < n; i++)
-		sum += va_arg(list, int);
-
-	va_end(list);
-
-	return (sum);
+	/**
+	*unsigned int i;
+	*va_list list;
+	*int sum;
+	*
+	*va_start(list, n);
+	*for (i = 0, sum = 0; i < n; i++)
+	*	sum += va_arg(list, int);
+	*
+	*va_end(list);
+	*
+	*return (sum);
 	*/
 
 	va_list nums;
