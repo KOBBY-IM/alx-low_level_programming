bc4261af07ce6ee584d69c2c6f616a20c73acf3d
KOBBY-IM <nyarkotheo2@gmail.com>
2023-03-23 18:33:29 +0000

    Task 0 more errors

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -24,15 +24,19 @@ int sum_them_all(const unsigned int n, ...)
 	*return (sum);
 	*/
 
-	va_list nums;
-	unsigned int index, sum = 0;
 
-	va_start(nums, n);
+	va_list valist;
+	unsigned int sum = 0, i;
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
+	/* validate valist and initialize */
+	if (n == 0)
+		return (0);
+	va_start(valist, n);
 
-	va_end(nums);
+	/* iterate through list, update sum, free list */
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
 
 	return (sum);
 }
