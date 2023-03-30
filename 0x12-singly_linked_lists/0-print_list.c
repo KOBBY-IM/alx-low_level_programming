c6400451204c901f6564ed2e0f76955293706937
KOBBY-IM <nyarkotheo2@gmail.com>
2023-03-30 21:09:09 +0000

    Changed %d to %u

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	current = h;
 	while (current != NULL)
 	{
-		printf("[%d] %s\n", current->len, current->str);
+		printf("[%u] %s\n", current->len, current->str);
 		current = current->next;
 		c++;
 	}
