decb2ecf5913e276901c9ea300cbbfb3ac2af9ef
KOBBY-IM <nyarkotheo2@gmail.com>
2023-04-04 13:07:21 +0000

    added description to code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,14 +13,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current = head;
 	unsigned int i = 0;
 
+	/*iterate list till it reaches nth node or end of list */
 	for (i = 0; i < index && current != NULL; i++)
 	{
 		current = current->next;
 	}
-	if (current == NULL)
+	if (current == NULL) /*if it reaches end of list b4 nth node, reutn null */
 	{
 		return (NULL);
 	}
 	else
-		return (current);
+		return (current); /* return pointer to nth node */
 }
