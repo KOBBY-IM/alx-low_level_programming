1d5072d3c3d11c8f60969cf2ba44ab62ecffab7f
KOBBY-IM <nyarkotheo2@gmail.com>
2023-07-26 21:46:40 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,19 +12,23 @@ def island_perimeter(grid):
         The perimeter of the island.
     """
 
-    perimeter = 0
-    height = len(grid)
-    width = len(grid[0])
-
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1
-
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
-
-    return perimeter
+     count = 0
+    max_grid = len(grid) > 100 if len(grid) == 100 else len(grid)
+    for row in range(max_grid):
+        max_row = len(grid[row]) > 100 \
+            if len(grid[row]) == 100 else len(grid[row])
+        for col in range(max_row):
+            if grid[row][col] == 1:
+                top = grid[row - 1][col] if row - 1 != -1 else 0
+                bot = grid[row + 1][col] if row + 1 != max_grid else 0
+                left = grid[row][col - 1] if col - 1 != -1 else 0
+                right = grid[row][col + 1] if col + 1 != max_row else 0
+                if top == 0:
+                    count += 1
+                if bot == 0:
+                    count += 1
+                if left == 0:
+                    count += 1
+                if right == 0:
+                    count += 1
+    return count
